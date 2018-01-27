0bb0aeba2a5233f7e26e710f1bdee6757fc88154
mgoodling15 <megoodling@davidson.edu>
2018-01-26 23:47:35 -0500

    Update user.java

--- user.java
+++ user.java
@@ -1,3 +1,11 @@
+import java.util.*;
+
+/**A custom class that represents a user object.
+ * 
+ * Stores all information needed for a user.
+ *
+ */
+
 public class User {
     String firstname;
     String lastname;
